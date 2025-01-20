3148f56b7e38ef1632c6b5eccfa6ab8c5b4d1f8d
Erik Öberg <erik.oberg@miun.se>
2025-01-20 11:13:32 +0100

    Add project files.

---
+++ Skistar/Controllers/FirendController1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skistar.Controllers;
+
+public class FirendController1 : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
