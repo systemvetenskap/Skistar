119c8254531423b82085317fb16405c97fb74b51
Erik Öberg <erik.oberg@miun.se>
2025-01-24 10:58:49 +0100

    startar identity

---
+++ Skistar/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Skistar.ViewModels;
+
+namespace Skistar.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Skistar/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Skistar.ViewModels;
+
+public class LoginViewModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
